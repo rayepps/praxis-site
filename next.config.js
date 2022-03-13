e60bbb00be4791956adc4ba2c27581e0cf2802f9
Ray Epps <rayharryepps@gmail.com>
2022-03-13 17:24:30 -0600

    point back to normal api address

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   crossOrigin: 'anonymous',
   env: {
     graphCmsUrl: 'https://api-us-west-2.graphcms.com/v2/ckz6cwva60jqu01xtbztmgcbz/master',
-    apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:7709' : 'https://fast-api.praxisco.us'
+    apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:7709' : 'https://api.praxisco.us'
   }
 }

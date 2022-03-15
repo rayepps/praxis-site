334737df982786a01d65f5ae0e1b92cdff1c915f
Ray Epps <rayharryepps@gmail.com>
2022-03-15 02:57:33 -0600

    improve footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,14 +3,13 @@ import { Center } from '../Layout'
 export default function Footer() {
   return (
     <div className="md:py-4 mt-28 text-center">
-      <span className="font-bold text-sm">
-        Email me us{' '}
-        <a className="text-yellow-400" href="mailto:ray@praxisco.us">
+      <span className="text-sm">
+        Follow us{' '}
+        <a className="text-yellow-400 font-bold" target="blank" href="https://twitter.com/praxiscous">
+          @praxiscous
+        </a> or email us directly at{' '}
+        <a className="text-yellow-400 font-bold" href="mailto:ray@praxisco.us">
           ray@praxisco.us
-        </a>{' '}
-        or message me at{' '}
-        <a className="text-yellow-400" target="blank" href="https://twitter.com/repsofsunshine">
-          @repsofsunshine
         </a>
       </span>
     </div>
